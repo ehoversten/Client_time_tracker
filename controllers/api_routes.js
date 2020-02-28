92d28613847fdf314d24974f4ad63b9a52173624
ehoversten <ehoversten@gmail.com>
2020-02-27 22:50:20 -0500

    seperating routes into controllers directory, testing creating and query to database

---
+++ controllers/api_routes.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+
+const db = require('../models');
+
+router.get('/', (req, res) => {
+    db.Client.find({}).then(data => {
+        res.json(data);
+    }).catch(err => {
+        res.status(500).json(err);
+    });
+});
+
+
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 var exphbs = require('express-handlebars');
 const mongoose = require('mongoose');
+const logger = require("morgan");
+const api_routes = require('./controllers/api_routes');
+const html_routes = require('./controllers/html_routes');
 
 const PORT = process.env.PORT || 3000;
 
@@ -9,6 +12,10 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/project_tracker
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
+
+// Bring in our Models
+const db = require("./models");
+
 
 // Initialize an express instance
 const app = express();
@@ -19,17 +26,35 @@ app.use(express.static("public"));
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
+app.use(logger("dev"));
+
 // Parse request body as JSON
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
  
 
+// *** TESTING *** //
+
+// db.Client.create({
+//         name: "Capital Music",
+//         contact: "Carol King"
+//     }).then(data => {
+//         console.log(data);
+//     }).catch(({ message }) => {
+//         console.log(message);
+//     });
+
+
 
 // -- ROUTES -- //
-app.get('/', (req, res) => {
-    // res.send("Hi There");
-    res.render('index');
-})
+// app.get('/', (req, res) => {
+//     // res.send("Hi There");
+//     res.render('index');
+// });
+
+
+app.use('/', html_routes);
+app.use('/api', api_routes);
 
 
 app.listen(PORT, () => {
